77be757217bdb8980092bd41ff812b258d97f2b6
Carlos Flores <carlos.flores@navya.tech>
2022-06-21 18:39:15 +0200

    Parallel threading dummy test.

--- src/main.py
+++ src/main.py
@@ -1,41 +1,4 @@
-import random
-
 import car_following
-from threading import *
-from time import *
-
-
-class Simulation(Thread):
-    def __init__(self, id):
-        self.id = id
-        self.rand_int = -1
-
-    def slow_function(self, name):
-        sleep(1.0)
-        self.rand_int = random.randint(0, 10)
-        print("      thread " + str(name) + " rand to send: " + str(self.rand_int))
-
-
-class HMI(Thread):
-    def __init__(self, id):
-        self.id = id
-
-    def slow_function(self, name, i):
-        print("      thread "+ str(name) + " received: " + str(i))
-        sleep(1.0)
-
-simulation = Simulation(0)
-hmi = HMI(1)
-
-
-for i in range(4):
-    t1 = Thread(target=simulation.slow_function, args=("alpha",))
-    t2 = Thread(target=hmi.slow_function, args=("beta", simulation.rand_int))
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
 
 
 # Init vehicles with random models, with probability of selecting a model type
